265a8813e2737827eda1884db35902275ae4cace
sashakmets <sashakmets@yandex.ru>
2015-06-20 09:53:35 +0300

    Собеседование по Java concurrency

---
+++ src/com/Concurrent/Stack.java
@@ -0,0 +1,43 @@
+package com.Concurrent;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Created by Саша on 20.06.2015.
+ * {@link http://www.duct-tape-architect.ru/?p=294}
+ * ************************************************
+ * 5) Напишите минимальный неблокирующий стек (всего два метода — push() и pop()).
+ */
+public class Stack {
+
+    private final AtomicReference<Element> headRef = new AtomicReference<Element>(null);
+
+    public void push(T value) {
+        Element newHead = new Element();
+        newHead.value = value;
+        Element oldHead;
+        do {
+            oldHead = headRef.get();
+            newHead.next = oldHead;
+        } while (!headRef.compareAndSet(oldHead, newHead));
+    }
+
+    public T pop() {
+        Element oldHead = null;
+        Element newHead = null;
+        do {
+            oldHead = headRef.get();
+            if (oldHead == null) {
+                return null;
+            }
+            newHead = oldHead.next;
+        } while (!headRef.compareAndSet(oldHead, newHead));
+        return oldHead.value;
+    }
+
+    public static class Element {
+        private T value;
+        private Element next;
+    }
+
+}
